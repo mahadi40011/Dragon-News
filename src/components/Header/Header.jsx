f31840c3d111f0a7427a3998911d0339930dca6f
mahadi40011 <mahadi40011@gmail.com>
2025-10-18 17:54:45 +0600

    install date-fns packge and display the current date

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from "../../assets/logo.png"
+import { format } from 'date-fns';
 
 const Header = () => {
   return (
-    <div className='flex flex-col gap-5 justify-center items-center'>
+    <div className='flex flex-col justify-center items-center mt-12'>
       <img className='w-[470px] h-15' src={logo} alt="" />
-      <p className='text-accent text-lg'>Journalism Without Fear or Favour</p>
+      <p className='text-accent text-lg mt-5 mb-2'>Journalism Without Fear or Favour</p>
+      <span className='text-accent font-semibold'>{format(new Date(), "EEEE, MMMM dd, yyyy")}</span>
     </div>
   );
 };
