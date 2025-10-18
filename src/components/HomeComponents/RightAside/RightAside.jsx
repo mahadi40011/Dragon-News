abe271257cc87620720ef7c9133ae1c31429f8a1
mahadi40011 <mahadi40011@gmail.com>
2025-10-19 00:56:52 +0600

    create a new component 'FineUs' and set it into RightAside.jsx

--- src/components/HomeComponents/RightAside/RightAside.jsx
+++ src/components/HomeComponents/RightAside/RightAside.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import SocialLogin from './SocialLogin';
+import FindUs from './FindUs';
 
 const RightAside = () => {
   return (
-    <div>
-      <SocialLogin>
-        
-      </SocialLogin>
+    <div className='space-y-8'>
+      <SocialLogin />
+      <FindUs/>
     </div>
   );
 };
