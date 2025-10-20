8ceea8e117339ea6ce04489c143299c2fa38be33
mahadi40011 <mahadi40011@gmail.com>
2025-10-20 12:52:41 +0600

    Firebase Authentication Listener added

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import auth from "../firebase/Firebase.config";
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  console.log(user)
+
   const createUser = (email, password) => {
-   return createUserWithEmailAndPassword(auth, email, password)
-  }
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
   const loginUser = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password)
-  }
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
   const authData = {
     user,
@@ -21,6 +27,14 @@ const AuthProvider = ({ children }) => {
     loginUser,
   };
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
